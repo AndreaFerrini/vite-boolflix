ed1dbee1eb1604e7a34a136605bf7b0ea9e87b3e
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-05-13 11:03:23 +0200

    inserite flag nella card per Languages

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -2,19 +2,37 @@
 
 export default {
     name: "SingleCard",
-    props: ['infoFilm']
+    props: ['infoFilm'],
+    methods: {
+        stampaBandiera() {
+            if (this.infoFilm.original_language == 'en') {
+                return '/public/images/en.png'
+            } else if (this.infoFilm.original_language == 'i') {
+                return '/public/images/it.png'
+            } else if (this.infoFilm.original_language == 'es') {
+                return '/public/images/es.png'
+            } else if (this.infoFilm.original_language == 'fr') {
+                return '/public/images/fr.png'
+            } else if (this.infoFilm.original_language == 'jpn') {
+                return '/public/images/jpn.png'
+            } else {
+                return '/'
+            }
+        }
+    }
 }
 
 </script>
 
 <template>
-    <div class="col-3">
+    <div class="col-3 p-3">
         <div class="card">
             <img src="#" class="card-img-top" :alt="infoFilm.original_title">
             <div class="card-body">
                 <h5 class="card-title">{{ infoFilm.original_title }}</h5>
                 <p class="card-text">{{ infoFilm.overview }}</p>
-                <h6>{{ infoFilm.original_language }}</h6>
+                <!-- <h6>{{ infoFilm.original_language }}</h6> -->
+                <img :src="stampaBandiera()" width="50" alt="">
                 <h6>{{ infoFilm.vote_average }}</h6>
             </div>
         </div>
